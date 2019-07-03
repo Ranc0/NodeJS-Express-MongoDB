48803313fe41c3bfabad43ad89502b2f69f6b005
Ranc0 <maieli.franco@gmail.com>
2019-07-03 16:10:28 -0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2312946332278410',
+        clientSecret: 'bf0db35dd6145884d377451357982d97'
+    }
 }
